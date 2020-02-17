c345fe8475665b7791f1cc6246da530bc2c0e06a
user <paul.tychyj@gmail.com>
2020-02-17 08:05:21 +0100

    suppression des " this -> " de linearInt.hpp - Paul

--- TP2/linearInt.hpp
+++ TP2/linearInt.hpp
@@ -13,7 +13,7 @@ private:
 public:
   linearInt(const std::vector<double> axe_x, const std::vector<double> axe_y) {
     int taille = axe_x.size();
-    this -> m_Xs = axe_x;
+    m_Xs = axe_x;
     double x1, x2;
     double y1, y2;
     for(int N = 0; N < taille-1; ++N) {
@@ -21,22 +21,16 @@ public:
       x2 = axe_x[N+1];
       y1 = axe_y[N];
       y2 = axe_y[N+1];
-      this-> m_As.push_back((y2-y1)/(x2-x1));
-      this-> m_Bs.push_back(y1);
+      m_As.push_back((y2-y1)/(x2-x1));
+      m_Bs.push_back(y1);
     }
-    this-> m_As.push_back((axe_y[0]-y2)/(1-x2));
-    this-> m_Bs.push_back(y2);
+    m_As.push_back((axe_y[0]-y2)/(1-x2));
+    m_Bs.push_back(y2);
   }
 
   float get_value(const double x) const {
     int indice = 0;
-    while(x > m_Xs[indice+1] && indice < m_Xs.size()-1){
-      ++indice;
-      // std::cout << "indice: " << indice << std::endl;
-      // std::cout << "m_Xs[indice]: " << m_Xs[indice];
-    }
-    // std::cout << std::endl << "m_As[indice]" << m_As[indice] << std::endl
-    //                        << "m_Bs[indice]" << m_Bs[indice] << std::endl;
+    while(x > m_Xs[indice+1] && indice < m_Xs.size()-1) ++indice;
     return (m_As[indice]*(x-m_Xs[indice]) + m_Bs[indice]);
   }
 };
